16a6b5c4c758dc649c14d59d2207fa510cd1f13f
Tuguldur.bayrtsolmon@gmail.com <togoldor.bayrtsolmon@gmail.com>
2022-01-30 15:27:08 +0800

    fixed hope

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import vue from '@vitejs/plugin-vue'
 //https://vitejs.dev/config
 export default defineConfig({
-    base:'/insideheadoftuugu/',
+    base:'../insideheadoftuugu/',
     plugins:[vue()],
     build: {
         chunkSizeWarningLimit: 1600,
