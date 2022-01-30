f739f0eb827d9580208a4c6ecef517bc9740b37e
Tuguldur.bayrtsolmon@gmail.com <togoldor.bayrtsolmon@gmail.com>
2022-01-30 15:16:39 +0800

    fixed

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-import * as THREE from 'three';
+import * as THREE from './js/three.module.js';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
 
