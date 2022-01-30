4b29c61a1db177155278dba44fa818bf78ef2a85
Tuguldur.bayrtsolmon@gmail.com <togoldor.bayrtsolmon@gmail.com>
2022-01-30 16:39:11 +0800

    last2

--- static/main.js
+++ static/main.js
@@ -1,4 +1,4 @@
-import './static/style.css';
+import '/static/style.css';
 
 import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
 import {OrbitControls} from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
